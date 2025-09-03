55a64c636dd2998148031c69050a7ad6787fe191
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-09-03 19:52:25 -0300

    projeto iniciado

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { LoginScreenComponent } from './user-module/login-screen/login-screen.component';
+
+export const routes: Routes = [ 
+    {
+      path: "login",
+      loadComponent: () =>  LoginScreenComponent
+    }
+];
