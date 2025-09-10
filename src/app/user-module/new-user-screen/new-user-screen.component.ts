19891f7aa4d29b1a1d0ba7e323129b9e2e49fc2c
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-09-09 21:51:34 -0300

    adicionando tela de cadastro

---
+++ src/app/user-module/new-user-screen/new-user-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user-screen',
+  imports: [],
+  templateUrl: './new-user-screen.component.html',
+  styleUrl: './new-user-screen.component.css'
+})
+export class NewUserScreenComponent {
+}
+
+
