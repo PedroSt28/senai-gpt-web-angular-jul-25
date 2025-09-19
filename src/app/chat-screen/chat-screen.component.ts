d597747f5fcea4822050de94ae9d9b325c8506e1
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-09-18 21:54:37 -0300

    Interaçao IA

--- src/app/chat-screen/chat-screen.component.ts
+++ src/app/chat-screen/chat-screen.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Type } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { firstValueFrom } from 'rxjs';
 
@@ -56,7 +56,7 @@ export class ChatScreenComponent { //constroi classes
         "Authorization": "Bearer " + localStorage.getItem("meuToken")
       }
 
-    }))
+    }));
 
     if (response) {
 
@@ -97,16 +97,66 @@ export class ChatScreenComponent { //constroi classes
 
     let novaMensagemUsuario = {
 
-
-      chatId: this.chatSelecionado ,
+      chatId: this.chatSelecionado.id,
       userId: localStorage.getItem("meuId"),
-      text:this.mensagemUsuario.value
+      text: this.mensagemUsuario.value,
+
+    };
+    // 1-- Salva as mensagens do usuario no banco de dados
+    let novaMensagemUsuarioResponse = await firstValueFrom(this.http.post("https://senai-gpt-api.azurewebsites.net/messages", novaMensagemUsuario, {   //post serve para mandar a mensagem 
+      headers: {
+
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("meuToken"),
+
+      }
+
+    }));
+    //atualiza as mensagens da tela
+    await this.onChatClick(this.chatSelecionado)
+
+    // 2 -- Enviar a mensagem para a IA responder
+    //Aestrutura o google que entregou!!!!!
+    let respostaIAResponse = await firstValueFrom(this.http.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", {
+      "contents": [
+        {
+          "parts": [
+            {
+              "text": this.mensagemUsuario.value + ". me de uma resposta ."
+            }
+          ]
+        }
+      ]
+    }, {
+      headers : {
+        "Content-Type" : "application/json",
+        "X-goog-api-key" : "AIzaSyDV2HECQZLpWJrqCKEbuq7TT5QPKKdLOdo"
+      }
+
+    })) as any;
+
+    let novaRespostaIA =  {
+
+    chatId:this.chatSelecionado.id,
+    userId:"chatbot",
+    text: respostaIAResponse.candidates[0].content.parts[0].text,
 
     }
+// 3-- salva a nova mensagem 
+ let novaRespostaIAResponse = await firstValueFrom(this.http.post("https://senai-gpt-api.azurewebsites.net/messages", novaRespostaIA, {   //post serve para mandar a mensagem 
+      headers: {
+
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("meuToken"),
+
+      }
+
+    }));
+
+    //atualiza as mensagens da tela
+    await this.onChatClick(this.chatSelecionado);
+
 
   }
 
 }
-
-
-
